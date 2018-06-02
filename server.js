69429906786f4b417924cc76db6950663982acc7
iamnikhilupadhyay <iamnikhilupadhyay1014@gmail.com>
2018-06-02 07:24:17 +0530

    v@0.0.0.1

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 var express = require('express');
 var path = require('path');
+var jade = require('jade');
 var bodyParser=require('body-parser');
 var logger = require('morgan');
 var stylus=require('stylus');
-var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+var mongoose = require('mongoose');
+
 var app = express();
+
+var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
 
 function compile(str, path) {
  return stylus(str).set('filename',path);
@@ -13,9 +18,9 @@ app.use(bodyParser.urlencoded({extended : true})); // to allow nested object ext
 
 app.use(bodyParser.json());// to catch incoming data
 
-app.engine('html', require('ejs').renderFile);
+//app.engine('html', require('ejs').renderFile);
 app.set('views',__dirname + '/server/views');
-app.set('view engine', 'html');
+app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(stylus.middleware(
  {
@@ -24,8 +29,19 @@ app.use(stylus.middleware(
  }
 ))
 
+mongoose.connect('mongodb://localhost:27017/multivision');// it will create db as multivision if not created
+
+var db = mongoose.connection;
+db.on('error',console.error.bind(console,'connection error..'));
+db.once('open', function callback() {
+    console.log('multivsion db opened');
+});
 
 app.use(express.static(path.join(__dirname+'/public')));
+
+app.get('/partials/:partialPath',(req,res)=> {
+  res.render('partials/'+ req.params.partialPath)
+});
 
 app.get('*', (req,res)=> {
  res.render('index');
